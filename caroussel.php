d2f5a2e17b8c95cfef0e84436a1de98a5bce0f51
el0ooo <eloichayer@gmail.com>
2024-04-18 16:03:57 -0400

    s10c2 - pause

--- caroussel.php
+++ caroussel.php
@@ -10,9 +10,16 @@ function eddym_enqueue(){
     $version_css = filemtime(plugin_dir_path( __FILE__ ) . "style.css");
     $version_js = filemtime(plugin_dir_path(__FILE__) . "js/carrousel.js");
 
-    wp_enqueue_style('em_plugin_carrousel_css', plugin_dir_url(__FILE__) . "style.css", array(), $version_css);
-    
-    wp_enqueue_script('em_plugin_carrousel_js', plugin_dir_url(__FILE__) ."js/carrousel.js", array(), $version_js, true);
+    wp_enqueue_style(   'em_plugin_carrousel_css',
+                     plugin_dir_url(__FILE__) . "style.css",
+                     array(),
+                     $version_css);
+
+    wp_enqueue_script(  'em_plugin_carrousel_js',
+                    plugin_dir_url(__FILE__) ."js/carrousel.js",
+                    array(),
+                    $version_js,
+                    true);
 }
 
 add_action('wp_enqueue_scripts', 'eddym_enqueue');
--- js/carrousel.js
+++ js/carrousel.js
@@ -1,3 +1,24 @@
 (function(){
+    let carrousel = document.querySelector('.carrousel');
+    let bouton = document.querySelector('.bouton__ouvrir');
+    let carrouselX = document.querySelector('.carrousel__x');
+    let carrouselFigure = document.querySelector('.carrousel__figure');
+    let galerie = document.querySelector('.galerie');
+    let galerieImage = galerie.querySelectorAll('img');
     
+    for(const elm of galerieImage){
+        let carrousel__img = document.createElement('img');
+        carrousel__img.classList.add('carrousel__img');
+        carrousel__img.src = elm.src;
+        
+        carrouselFigure.appendChild(carrousel__img);
+    }
+
+    bouton.addEventListener('mousedown', function(){
+        carrousel.classList.add('carrousel--ouvrir');
+    });
+
+    carrouselX.addEventListener('mousedown', function(){
+        carrousel.classList.remove('carrousel--ouvrir');
+    });
 })()
