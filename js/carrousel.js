a7b5b65599846fcc0860476763889ee7c8d87cee
el0ooo <eloichayer@gmail.com>
2024-04-18 17:05:42 -0400

    s10c2 - fin du labo5

--- js/carrousel.js
+++ js/carrousel.js
@@ -22,12 +22,19 @@
         carrousel__radio.classList.add('carrousel__radio');
         carrousel__radio.dataset.index = indexRadio;
         indexRadio++;
-
+        carrousel__radio.addEventListener('change', function(){
+            let index = this.dataset.index;
+            let carrousel__img = carrousel.querySelectorAll('.carrousel__img');
+            for(const elm of carrousel__img){
+                elm.classList.remove('image--active');
+            }
+            carrousel__img[index].classList.add('image--active');
+        });
         carrouselForm.appendChild(carrousel__radio);
 
     }
-    
-    carrouselForm.addEventListener('change', function(carrousel__radio){
+
+    carrouselForm.addEventListener('change', function(){
         if(carrousel__radio.checked == true){
             carrousel__img.classList.add('image--active');
         }else {
