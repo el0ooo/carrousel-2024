2607c56e477a6f6009b8c709923f3e8bccaf2444
el0ooo <eloichayer@gmail.com>
2024-04-18 17:08:02 -0400

    s10c2 - fin labo 5 pour de vrai

--- js/carrousel.js
+++ js/carrousel.js
@@ -13,16 +13,22 @@
         let carrousel__img = document.createElement('img');
         carrousel__img.classList.add('carrousel__img');
         carrousel__img.src = elm.src;
-        
+        if(indexRadio == 0){
+            carrousel__img.classList.add('image--active');
+        }
         carrouselFigure.appendChild(carrousel__img);
 
         let carrousel__radio = document.createElement('input');
         carrousel__radio.type = 'radio';
         carrousel__radio.name = 'carrousel__radio';
         carrousel__radio.classList.add('carrousel__radio');
+        if(indexRadio == 0){
+            carrousel__radio.checked = true;
+        }
         carrousel__radio.dataset.index = indexRadio;
         indexRadio++;
         carrousel__radio.addEventListener('change', function(){
+
             let index = this.dataset.index;
             let carrousel__img = carrousel.querySelectorAll('.carrousel__img');
             for(const elm of carrousel__img){
