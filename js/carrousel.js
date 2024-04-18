a2d0803683cd1fb012640918af31bb6405e6f432
el0ooo <eloichayer@gmail.com>
2024-04-18 16:58:14 -0400

    s10c2 - debut labo5

--- js/carrousel.js
+++ js/carrousel.js
@@ -3,16 +3,38 @@
     let bouton = document.querySelector('.bouton__ouvrir');
     let carrouselX = document.querySelector('.carrousel__x');
     let carrouselFigure = document.querySelector('.carrousel__figure');
+    let carrouselForm = document.querySelector('.carrousel__form');
     let galerie = document.querySelector('.galerie');
     let galerieImage = galerie.querySelectorAll('img');
     
+    let indexRadio = 0;
+
     for(const elm of galerieImage){
         let carrousel__img = document.createElement('img');
         carrousel__img.classList.add('carrousel__img');
         carrousel__img.src = elm.src;
         
         carrouselFigure.appendChild(carrousel__img);
+
+        let carrousel__radio = document.createElement('input');
+        carrousel__radio.type = 'radio';
+        carrousel__radio.name = 'carrousel__radio';
+        carrousel__radio.classList.add('carrousel__radio');
+        carrousel__radio.dataset.index = indexRadio;
+        indexRadio++;
+
+        carrouselForm.appendChild(carrousel__radio);
+
     }
+    
+    carrouselForm.addEventListener('change', function(carrousel__radio){
+        if(carrousel__radio.checked == true){
+            carrousel__img.classList.add('image--active');
+        }else {
+            carrousel__img.classList.remove('image--active');
+        }
+    });
+
 
     bouton.addEventListener('mousedown', function(){
         carrousel.classList.add('carrousel--ouvrir');
